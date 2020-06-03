4e02a3752d6590810e6ba656f51045e233b9286d
ghelfer <ghelfer@gmail.com>
2020-06-03 01:03:10 -0300

    set bubblesort jni

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -8,18 +8,39 @@ Java_com_ghelfer_appjni_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     return env->NewStringUTF(hello.c_str());
 }
-extern "C"
-JNIEXPORT jint JNICALL
+
+
+extern "C" JNIEXPORT jint JNICALL
 Java_com_ghelfer_appjni_MainActivity_sumValues(JNIEnv *env, jobject thiz, jint a, jint b) {
     return a + b;
 }extern "C"
+
 JNIEXPORT jfloat JNICALL
 Java_com_ghelfer_appjni_MainActivity_getFahreinheit(JNIEnv *env, jobject thiz, jfloat celcius) {
 
     jfloat result = celcius*1.8 + 32;
     return  result;
-}extern "C"
-JNIEXPORT jintArray JNICALL
+}
+
+extern "C" JNIEXPORT jintArray JNICALL
 Java_com_ghelfer_appjni_MainActivity_getBubbleSort(JNIEnv *env, jobject thiz, jintArray values) {
-    // TODO: implement getBubbleSort()
+    const jsize TAM = env->GetArrayLength(values);
+    jint *array = env->GetIntArrayElements(values, NULL);
+    jintArray newArray = env->NewIntArray(TAM);
+
+    jint i = 0, j = 0,  aux = 0,  k = TAM - 1 ;
+
+    for(i = 0; i < TAM; i++)    {
+        for(j = 0; j < k; j++)        {
+            if(array[j] > array[j+1])            {
+                aux = array[j];
+                array[j] = array[j+1];
+                array[j+1]=aux;
+            }
+        }
+        k--;
+    }
+    env->SetIntArrayRegion(newArray, 0, TAM, array);
+
+    return newArray;
 }
--- app/src/main/java/com/ghelfer/appjni/MainActivity.java
+++ app/src/main/java/com/ghelfer/appjni/MainActivity.java
@@ -20,6 +20,23 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+
+        TextView tv1 = findViewById(R.id.sample_sum);
+        tv1.setText("soma = " + sumValues(3, 4));
+
+        TextView tv2 = findViewById(R.id.sample_conv);
+        tv2.setText("Fahreinheit = " + getFahreinheit(12));
+
+        TextView tv3 = findViewById(R.id.sample_sort);
+
+        int [] a = {3,5,1,6,9,4};
+        int [] sorted = getBubbleSort(a);
+        String  res = "";
+        for (int x=0; x < sorted.length;x++){
+            res += sorted[x] + ", ";
+        }
+
+        tv3.setText("sorting = " + res);
     }
 
     /**
